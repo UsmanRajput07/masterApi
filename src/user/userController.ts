7c3a747fe9378aced5d9008b944cc904179dccae
UsmanRao <bcawala3@gmail.com>
2024-10-19 16:52:04 +0530

    hased password

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import bcrypt from "bcrypt";
+import userModal from "./userModal";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const {name, email, password} = req.body;
@@ -8,6 +10,14 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
         const error = createHttpError(400, 'All fields are required');
         return next(error);
     }
+
+    //  database check
+    const user = await userModal.findOne({email});
+    if(user) {
+        const error = createHttpError(400, 'User already exists');
+        return next(error);
+    }
+    const hasshedPassword = await bcrypt.hash(password, 10);
     res.json({ message: 'hello world', name, email, password });
 };
 
