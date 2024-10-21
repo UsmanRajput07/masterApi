197e2d056575414e8a17606dd8c1b37a5d8419ff
UsmanRao <bcawala3@gmail.com>
2024-10-21 21:57:34 +0530

    user register complete

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,8 @@ dotenvConfig();
 const _config = {
   port: process.env.PORT || 3000,
   MongoDbUrl: process.env.MONGO_URL,
-  env: process.env.NODE_ENV
+  env: process.env.NODE_ENV,
+  jwtSecret: process.env.JWT_SECRET,
 };
 
 export const config = Object.freeze(_config);
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,23 +2,51 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import bcrypt from "bcrypt";
 import userModal from "./userModal";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
+import { user } from "./userTypes";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
-    const {name, email, password} = req.body;
+    const { name, email, password } = req.body;
 
-    if(!name || !email || !password) {
+    if (!name || !email || !password) {
         const error = createHttpError(400, 'All fields are required');
         return next(error);
     }
 
+
+
     //  database check
-    const user = await userModal.findOne({email});
-    if(user) {
-        const error = createHttpError(400, 'User already exists');
-        return next(error);
+    try {
+        const user = await userModal.findOne({ email });
+        if (user) {
+            const error = createHttpError(400, 'User already exists');
+            return next(error);
+        }
     }
-    const hasshedPassword = await bcrypt.hash(password, 10);
-    res.json({ message: 'hello world', name, email, password });
+    catch (err) {
+        return next(createHttpError(500, 'Error while checking user in database'));
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    let newUser: user
+    try {
+        newUser = await userModal.create({
+            name,
+            email,
+            password: hashedPassword
+        })
+    }
+    catch (err) {
+        return next(createHttpError(500, 'Error while creating  user in database'));
+
+    }
+
+
+    //  jwt token generate
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, { expiresIn: '7d' });
+
+    res.json({ message: 'User created successfully', accesToken: token });
 };
 
 export {
